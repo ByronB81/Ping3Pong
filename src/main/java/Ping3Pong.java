b61b6cb473bd324a292e470a51e0b3df1b10bfe7
Byron <byronblocker@gmail.com>
2017-05-22 10:19:41 -0700

    add first test working

---
+++ src/main/java/Ping3Pong.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ping3Pong{
+
+  public List<Object> runPing3Pong(int countUpTo) {
+    List<Object> result = new ArrayList<Object>();
+    result.add(1);
+    return result;
+  }
+}
--- src/test/java/Ping3PongTest.java
+++ src/test/java/Ping3PongTest.java
@@ -14,4 +14,14 @@ public class Ping3PongTest {
     assertEquals(expectedOutput, testPing3Pong.runPing3Pong(1));
 
   }
+
+  @Test
+  public void runPing3Pong_countUpToNumber_ArrayList(){
+    Ping3Pong testPing3Pong = new Ping3Pong();
+    List<Object> expectedOutput = new ArrayList<Object>();
+    expectedOutput.add(1);
+    expectedOutput.add(2);
+    assertEquals(expectedOutput, testPing3Pong.runPing3Pong(2));
+  }
+
 }
