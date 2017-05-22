af6eeea8f1d9ced7d6571583a72fe2dde4b8dc8f
Byron <byronblocker@gmail.com>
2017-05-22 11:00:14 -0700

    pass all tests and app now works

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,15 @@
+import java.io.Console;
+import java.util.List;
+
 public class App {
   public static void main(String[] args) {
-
-    System.out.println("Totes werkin', yo!");
+    Console console = System.console();
+    System.out.println("Enter a number");
+    String stringUserNumber = console.readLine();
+    int intUserNumber = Integer.parseInt(stringUserNumber);
+    Ping3Pong pingPong = new Ping3Pong();
+    List<Object> pingPongResult = pingPong.runPing3Pong(intUserNumber);
+    System.out.println(pingPongResult);
 
   }
 }
